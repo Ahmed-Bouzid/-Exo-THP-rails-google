eaf531b291a103ad210f15bc72f03605b48ec3ec
jihane <thp@jihane.fr>
2018-08-08 14:36:49 +0200

    add vendor boostrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,9 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require popper
+//= require bootstrap
 
 function danger(){
   alert("kikou JavaScript marche !")
